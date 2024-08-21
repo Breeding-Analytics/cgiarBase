1f6b683aa67fbf413c20209012929768f30dc22d
Raul Eyzaguirre <reyzaguirre@gmail.com>
2024-08-21 10:57:17 -0500

    Functions to round numbers in data.frames and data.tables in the Rmd output

---
+++ R/roundOutputNumbers.R
@@ -0,0 +1,43 @@
+# Function to give format to a data.frame
+round.df <- function(DT, numRound) {
+  data.frame(lapply(X = DT,
+                    FUN = function(x) {
+                      if(is.numeric(x)){
+                        n1 <- lapply(x, function(x) nchar(strsplit(as.character(x), "\\.")[[1]][1]))
+                        n1 <- min(unlist(n1), na.rm = TRUE)
+                        if (n1 >= numRound) {
+                          round(x, 0)
+                        } else {
+                          if (min(x, na.rm = TRUE) >= 1) {
+                            round(x, numRound - n1)
+                          } else {
+                            tmp <- signif(x, numRound)
+                            n2 <- lapply(tmp, function(x) nchar(strsplit(as.character(x), "\\.")[[1]][2]))
+                            n2 <- max(unlist(n2), na.rm = TRUE)
+                            round(x, n2)
+                          }
+                        }
+                      } else {
+                        x
+                      }
+                    }))
+}
+
+# Function to give format to a datatable
+round.dt <- function(table, numRound) {
+  x <- table$x$data
+  numericColNames <- unlist(lapply(x, is.numeric))
+  for (i in 1:length(numericColNames)) {
+    if (numericColNames[i]) {
+      n1 <- lapply(x[, i], function(x) nchar(strsplit(as.character(x), "\\.")[[1]][1]))
+      n1 <- unlist(n1)
+      for (j in 1:dim(x)[1]) {
+        if (n1[j] >= numRound)
+          table$x$data[j, names(numericColNames[i])] <- round(x[j, names(numericColNames[i])], 0)
+        if (n1[j] < numRound)
+          table$x$data[j, names(numericColNames[i])] <- signif(x[j, names(numericColNames[i])], numRound)
+      }
+    }
+  }
+  table
+}
