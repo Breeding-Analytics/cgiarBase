23a7b33a4c22d5e7049f593d5866e8836f4bf4e4
covaruber <cova_ruber@live.com.mx>
2024-08-23 10:24:38 +0800

    mtalmm covariates improve

--- R/formLme4.R
+++ R/formLme4.R
@@ -12,6 +12,7 @@ goodLevels <- function(object, analysisId, includeCovars=TRUE){
   }
   # add missing weather
   weather <- cgiarPipeline::summaryWeather(object)# object$metadata$weather
+  weather$trait <- gsub(" ","",gsub("[[:punct:]]", "", weather$trait))
   if(nrow(weather) > 0){
     weather$traitParameter <- paste(weather$trait, weather$parameter, sep="_")
     wide <- reshape(weather[,-which(colnames(weather)%in%c("trait","parameter"))], direction = "wide", idvar = "environment",
@@ -19,12 +20,12 @@ goodLevels <- function(object, analysisId, includeCovars=TRUE){
     colnames(wide) <- gsub("value_","",colnames(wide))
     predictions <- merge(predictions, wide, by="environment", all.x = TRUE)
   }
-  
+
   # extract
   availableTraits <- intersect( metaPheno[which(metaPheno$parameter %in% c("trait")),"value"], unique(predictions$trait) )
   factorPerTrait <- vector(mode="list", length = length(availableTraits)); names(factorPerTrait) <- availableTraits
   availableFactors <- metaPheno[which(metaPheno$parameter %in% c("environment","year","season","location","trial","study","management")),"parameter"]
-  
+
   for(iTrait in availableTraits){ # iTrait = availableTraits[1]
     provPred <- predictions[which(predictions$trait == iTrait),]
     provPred <- provPred[which(!is.na(provPred$predictedValue)),]
@@ -44,6 +45,7 @@ formLme4 <- function(input0,object, analysisId, trait){
   # if(is.null(metaPheno)){stop("Metadata for phenotypes needs to be provided.", call. = FALSE)}
   metaPheno <- object$metadata$pheno
   weather <- cgiarPipeline::summaryWeather(object)# object$metadata$weather # object$metadata$weather
+  weather$trait <- gsub(" ","",gsub("[[:punct:]]", "", weather$trait))
   predictions <- object$predictions[which(object$predictions$analysisId == analysisId ),]
   '%!in%' <- function(x,y)!('%in%'(x,y))
   if(nrow(weather) > 0){
@@ -69,9 +71,9 @@ formLme4 <- function(input0,object, analysisId, trait){
     formulas <- loadingsEffects <- list()
     for(i in 1:length(input0)){ # for each effect to fit   i=1
       input <- input0[[i]]
-      
+
       if( is.null(input$left) ){ # fixed effect
-        
+
         if(input$center == ""){ # fixed effect
           if(is.null(input$right)){
             warning(paste("The term",i, "will be ignored since it was misspecified. Empty intercept and empty slope"))
@@ -81,9 +83,9 @@ formLme4 <- function(input0,object, analysisId, trait){
         }else{ # | or  ||
           warning(paste("The term",i, "will be ignored since it was misspecified. Empty intercept but structure specified"))
         }
-        
+
       }else if( length(input$left) == 1 ){ # only one term in the left side of the equation
-        
+
         if(input$left == "0"){ # invalid intercept
           warning(paste("The term",i, "will be ignored since it was misspecified. Empty in both sides of the equation"))
         }else if(input$left == "1"){ # simple intercept
@@ -93,7 +95,7 @@ formLme4 <- function(input0,object, analysisId, trait){
             input$center <- "|"
             warning(paste("The term",i, "will shift from || to | since it was misspecified."))
           }else{ # |
-            
+
             newLeftCol <- paste(input[["left"]], collapse = "_")
             if(newLeftCol == "1"){ # simple structure
               formulas[[i]] <-  paste( "( ", newLeftCol, input[["center"]], paste(input[["right"]], collapse=":") ,")" )
@@ -107,10 +109,10 @@ formLme4 <- function(input0,object, analysisId, trait){
                 for(j in 1:ncol(Z)){predictionsTrait[,colnames(Z)[j]] <- Z[,j]}
                 formulas[[i]] <-  paste( "( 0 +", paste(colnames(Z), collapse = " + "), input[["center"]], paste(input[["right"]], collapse=":") ,")" )
               }else{ # FA model
-                
+
                 predictionsTrait[, newLeftCol] <- apply( predictionsTrait[ , setdiff( input$left, c("0","1")), drop=FALSE ] , 1 , function(x){paste(na.omit(x), collapse = "-")}  )
                 predictionsTrait[ which(predictionsTrait[,newLeftCol] == ""), newLeftCol] <- NA
-                
+
                 H0 <- reshape(predictionsTrait[,c(input$right, newLeftCol, "predictedValue" )], direction = "wide", idvar = input$right,
                               timevar = newLeftCol, v.names = "predictedValue", sep= "_")
                 H0 <- apply(H0[,2:ncol(H0),drop=FALSE],2,sommer::imputev)
@@ -119,30 +121,30 @@ formLme4 <- function(input0,object, analysisId, trait){
                 colnames(Z) <- paste(colnames(Z), newLeftCol, sep="_")
                 colnames(Z) <- gsub(" ","", colnames(Z))
                 for(j in 1:ncol(Z)){predictionsTrait[,colnames(Z)[j]] <- Z[,j]}
-                
+
                 formulas[[i]] <-  paste( "( 0 +", paste(paste0("PC",1:input$nPC), collapse = " + "), input[["center"]], paste(input[["right"]], collapse=":") ,")" )
               }
             }
-            
+
           }
         }else{ # complex intercept (other than "1")
-          
+
           if(input$center == ""){
             warning(paste("The term",i, "will be ignored since it was misspecified. Intercept specified but no structure specified"))
           }else{ # |
-            
+
             columnClassLeft <- columnClass[setdiff( input$left, c("0","1"))]
             leftFactor <- names(columnClassLeft[which(columnClassLeft %in% c("character","factor") )])
             leftNumeric <- names(columnClassLeft[which(columnClassLeft %in% c("integer","numeric") )])
-            
+
             newLeftCol <- paste(input[["left"]], collapse = "_")
-            
+
             if(newLeftCol == "1"){ # simple structure
               formulas[[i]] <-  paste( "( ", newLeftCol, input[["center"]], paste(input[["right"]], collapse=":") ,")" )
             }else{ # complex structure
               if(input$nPC == 0){ # no FA
-                
-                if( length(leftFactor) > 0 ){  
+
+                if( length(leftFactor) > 0 ){
                   predictionsTrait[, newLeftCol] <- apply( predictionsTrait[ , input[["left"]], drop=FALSE ] , 1 , function(x){paste(na.omit(x), collapse = "-")}  )
                   predictionsTrait[ which(predictionsTrait[,newLeftCol] == ""), newLeftCol] <- NA
                   if(length(unique(na.omit(predictionsTrait[, newLeftCol]))) > 1){
@@ -154,64 +156,64 @@ formLme4 <- function(input0,object, analysisId, trait){
                     colnames(Z) <- paste0("L.",unique(na.omit(predictionsTrait[, newLeftCol])) )
                     colnames(Z) <- gsub(" ","", colnames(Z))
                   }
-                  if( length(leftNumeric) > 0 ){  
+                  if( length(leftNumeric) > 0 ){
                     Z <- cbind(Z, predictionsTrait[, leftNumeric, drop=FALSE])
                     colnames(Z) <- gsub(" ","", colnames(Z))
                   }
                 }else{
-                  if( length(leftNumeric) > 0 ){  
+                  if( length(leftNumeric) > 0 ){
                     Z <- predictionsTrait[, leftNumeric, drop=FALSE]
                     colnames(Z) <- gsub(" ","", colnames(Z))
                   }
                 }
                 for(j in 1:ncol(Z)){predictionsTrait[,colnames(Z)[j]] <- Z[,j]}
                 formulas[[i]] <-  paste( "( 0 +", paste(colnames(Z), collapse = " + "), input[["center"]], paste(input[["right"]], collapse=":") ,")" )
-                
+
               }else{ # FA model
-                
+
                 predictionsTrait[, newLeftCol] <- apply( predictionsTrait[ , setdiff( input$left, c("0","1")), drop=FALSE ] , 1 , function(x){paste(na.omit(x), collapse = "-")}  )
                 predictionsTrait[ which(predictionsTrait[,newLeftCol] == ""), newLeftCol] <- NA
-                
+
                 H0 <- reshape(predictionsTrait[,c(input$right, newLeftCol, "predictedValue" )], direction = "wide", idvar = input$right,
                               timevar = newLeftCol, v.names = "predictedValue", sep= "_")
                 H0 <- apply(H0[,2:ncol(H0),drop=FALSE],2,sommer::imputev)
                 colnames(H0) <- gsub("predictedValue_","",colnames(H0))
-                
+
                 rrFactor <- lme4breeding::rrm(predictionsTrait[,newLeftCol], H = H0, nPC = input$nPC, returnGamma = TRUE)
-                
+
                 Z <- with(predictionsTrait, lme4breeding::smm( rrFactor$Zstar ) )
                 colnames(Z) <- paste(colnames(Z), newLeftCol, sep="_")
                 colnames(Z) <- gsub(" ","", colnames(Z))
-                
+
                 for(j in 1:ncol(Z)){predictionsTrait[,colnames(Z)[j]] <- Z[,j]}
                 formulas[[i]] <- paste( "( 0 +", paste(colnames(Z), collapse = " + "), input[["center"]], paste(input[["right"]], collapse=":") ,")" )
                 loadingsEffects[[paste(input[["right"]], collapse=":")]] <- rrFactor$Gamma
               } # ond of nPC
             } # end of complex structure
           }# end of | structure
-          
+
         }
-        
+
       }else{ # multiple terms in the left side (intercepts)
-        
+
         if( length( setdiff( input$left, c("0","1")) ) == 0 ){ # misspecified
           warning(paste("The term",i, "will be ignored since it was misspecified. Intercept 0 + 1 is not allowed"))
         }else{
           if(input$center == ""){
             warning(paste("The term",i, "will be ignored since it was misspecified. Intercept specified but no structure specified"))
           }else{ # || or  |
-            
+
             if(is.null(input$right)){ # no right side
               warning(paste("The term",i, "will be ignored since it was misspecified. Intercept and structure specified but no right side"))
             }else{
-              
+
               columnClassLeft <- columnClass[setdiff( input$left, c("0","1"))]
               leftFactor <- names(columnClassLeft[which(columnClassLeft %in% c("character","factor") )])
               leftNumeric <- names(columnClassLeft[which(columnClassLeft %in% c("integer","numeric") )])
-              
+
               newLeftCol <- paste( setdiff( input$left, c("0","1")) , collapse = "_")
               if(input$nPC == 0){ # no FA
-                if( length(leftFactor) > 0 ){  
+                if( length(leftFactor) > 0 ){
                   predictionsTrait[, newLeftCol] <- apply( predictionsTrait[ , setdiff( input$left, c("0","1")), drop=FALSE ] , 1 , function(x){paste(na.omit(x), collapse = "-")}  )
                   predictionsTrait[ which(predictionsTrait[,newLeftCol] == ""), newLeftCol] <- NA
                   if(length(unique(na.omit(predictionsTrait[, newLeftCol]))) > 1){
@@ -223,12 +225,12 @@ formLme4 <- function(input0,object, analysisId, trait){
                     colnames(Z) <- paste0( "L.", unique(na.omit(predictionsTrait[, newLeftCol])) )
                     colnames(Z) <- gsub(" ","", colnames(Z))
                   }
-                  if( length(leftNumeric) > 0 ){  
+                  if( length(leftNumeric) > 0 ){
                     Z <- cbind(Z, predictionsTrait[, leftNumeric, drop=FALSE])
                     colnames(Z) <- gsub(" ","", colnames(Z))
                   }
                 }else{
-                  if( length(leftNumeric) > 0 ){  
+                  if( length(leftNumeric) > 0 ){
                     Z <- predictionsTrait[, leftNumeric, drop=FALSE]
                     colnames(Z) <- gsub(" ","", colnames(Z))
                   }
@@ -236,13 +238,14 @@ formLme4 <- function(input0,object, analysisId, trait){
                 for(j in 1:ncol(Z)){predictionsTrait[,colnames(Z)[j]] <- Z[,j]}
                 formulas[[i]] <-  paste( "( 0 +", paste(colnames(Z), collapse = " + "), input[["center"]], paste(input[["right"]], collapse=":"),")" )
               }else{ # FA model
-                
+
                 predictionsTrait[, newLeftCol] <- apply( predictionsTrait[ , setdiff( input$left, c("0","1")), drop=FALSE ] , 1 , function(x){paste(na.omit(x), collapse = "-")}  )
                 predictionsTrait[ which(predictionsTrait[,newLeftCol] == ""), newLeftCol] <- NA
-                
+
                 H0 <- reshape(predictionsTrait[,c(input$right, newLeftCol, "predictedValue" )], direction = "wide", idvar = input$right,
                               timevar = newLeftCol, v.names = "predictedValue", sep= "_")
                 H0 <- apply(H0[,2:ncol(H0),drop=FALSE],2,sommer::imputev)
+                # H0 <- sommer::corImputation(H0)
                 colnames(H0) <- gsub("predictedValue_","",colnames(H0))
                 Z <- with(predictionsTrait, lme4breeding::smm(lme4breeding::rrm(predictionsTrait[,newLeftCol], H = H0, nPC = input$nPC)) )
                 colnames(Z) <- paste(colnames(Z), newLeftCol, sep="_")
@@ -252,14 +255,14 @@ formLme4 <- function(input0,object, analysisId, trait){
                 loadingsEffects[[paste(input[["right"]], collapse=":")]] <- rrFactor$Gamma
               } # ond of nPC
               # } # end of complex structure
-              
+
             }# end of right-side exist
-            
+
           } # end of | or ||
         }# end of multiple terms in left side properly specified
-        
+
       }# end of multiple terms in the left side
-      
+
     } # end of for each effect
     form[[iTrait]] <- paste( unique(unlist(formulas)), collapse = " + ")
     preds[[iTrait]] <- predictionsTrait
