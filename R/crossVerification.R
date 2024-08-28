d7394146fc48113fbf72f306d6c82df2aae33262
covaruber <cova_ruber@live.com.mx>
2024-08-28 16:30:13 +0800

    new cross verification

--- R/crossVerification.R
+++ R/crossVerification.R
@@ -1,6 +1,8 @@
 
 
-crossVerification <- function(Mf,Mm,Mp, ploidy=2){
+crossVerification <- function(Mf,Mm,Mp, 
+                              Mexp=NULL,
+                              ploidy=2){
   
   consensus <- function(M, FUN){
     # take a set of samples and get the consensus sequence
@@ -12,43 +14,44 @@ crossVerification <- function(Mf,Mm,Mp, ploidy=2){
   if(any(!is.matrix(Mf) | !is.matrix(Mm) | !is.matrix(Mp) )){
     stop("One or more of the provided arguments is not a matrix", call. = FALSE)
   }
-  Mf1 <- (Mf + Mm)/2 #apply(rbind(Mf,Mm),2,mean) # expected genotype
+  if(is.null(Mexp)){ Mexp <- (Mf + Mm)/2 } # expected genotype
+  
   informativeMarkers <- which( apply(rbind(Mf,Mm),2,var) > 0 ) # markers with variance in parents at the pop level
 
   # matrix to save results
-  resultMatch <- Matrix::Matrix(0, nrow = nrow(Mf1), ncol=ncol(Mf1))
-  # matching matrix
-  match1 <- abs(Mf1 - Mp)
+  resultMatch <- Matrix::Matrix(0, nrow = nrow(Mexp), ncol=ncol(Mexp))
+  # matching matrix between expected and progeny
+  match1 <- abs(Mexp - Mp)
   
   # when both expected and realized progeny have same call is a full match
   resultMatch[which(match1 == 0, arr.ind = TRUE)]=1 
   # when expected and realized progeny was of the form P1 x P2 = 1 x 0
-  # expected progeny could be either 0 or 1 so Mf1=0.5 so if Mp=0 or Mp=1 
-  # Mf1 - Mp will be 0.5 or -0.5
+  # expected progeny could be either 0 or 1 so Mexp=0.5 so if Mp=0 or Mp=1 
+  # Mexp - Mp will be 0.5 or -0.5
   # we get a 0.5, it is a match but we have some uncertainty
   resultMatch[which(match1 == (0.5*(ploidy/2)), arr.ind = TRUE)]=0.5
   # when expected and realized progeny was of the form P1 x P2 = 2 x 1
-  # expected progeny could be either 2 or 1 so Mf1=1.5 so if Mp=2 or Mp=1 
-  # Mf1 - Mp will be 0.5 or -0.5
+  # expected progeny could be either 2 or 1 so Mexp=1.5 so if Mp=2 or Mp=1 
+  # Mexp - Mp will be 0.5 or -0.5
   # we get a 0.5, it is a match but we have some uncertainty
   
   # when expected and realized progeny was of the form P1 x P2 = 1 x 1
-  # expected progeny could be either 2 or 1 or 0 so Mf1=1 so if Mp=2 or Mp=1 or Mp=0
-  # Mf1 - Mp will be 1 or 0 or 0, no way to track it, intead we go back to those
+  # expected progeny could be either 2 or 1 or 0 so Mexp=1 so if Mp=2 or Mp=1 or Mp=0
+  # Mexp - Mp will be 1 or 0 or 0, no way to track it, intead we go back to those
   # specific cases and assign directly a 0.25 probability
   # we get a 0.25, it is a match but we have some uncertainty
-  resultMatch[which(Mf1 == (1*(ploidy/2)), arr.ind = TRUE)]=0.25
+  resultMatch[which(Mexp == (1*(ploidy/2)), arr.ind = TRUE)]=0.25
   
-  # compute metrics
+  # compute metrics for individuals
   probMatch <- apply(resultMatch,1,function(x){sum(x)/length(x)})  # proportion of markers matching
   heteroMp <- apply(Mp,1,function(x){length(which(x == 1))/length(x)}) # heterozigosity found in progeny
-  heteroMf1 <- apply(Mf1,1,function(x){length(which(x == 1))/length(x)}) #heterozigosity expected in progeny
+  heteroMf1 <- apply(Mexp,1,function(x){length(which(x == 1))/length(x)}) #heterozigosity expected in progeny
   heteroDeviation <- abs(heteroMf1 - heteroMp) # deviation from expectedheterosigosity
   propComplete <- apply(Mp,1,function(x){length(which(!is.na(x)))/length(x)}) # proportion of complete cases
-  
   res <- data.frame(designation=rownames(Mp),probMatch,heteroMp,heteroMf1,heteroDeviation,propComplete)
   rownames(res) <- NULL
-  return(list(metrics=res,matchMat=resultMatch))
+  # compute metrics for markers
+  return(list(metricsInd=res,matchMat=resultMatch))
   
 }
 
