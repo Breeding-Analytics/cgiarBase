327f1109dace3bfec73003f4704ad02ec6a54773
covaruber <cova_ruber@live.com.mx>
2024-09-10 22:02:56 +0700

    genotype summaries

--- R/summaryGeno.R
+++ R/summaryGeno.R
@@ -4,10 +4,10 @@ summaryGeno <- function(modifications ){
   # xx <- xx[which(xx$analysisId == analysisId),]
   suppressedMarkers <- length(which(is.na(modifications$row) & !is.na(modifications$col) & is.na(modifications$value) ))
   suppressedInds <- length(which(!is.na(modifications$row) & is.na(modifications$col) & is.na(modifications$value) ))
-  suppressedCells <- length(which(!is.na(modifications$row) & !is.na(modifications$col) & is.na(modifications$value) )) + (suppressedInds*ncol(object$data$geno)) + (suppressedMarkers*nrow(object$data$geno))
+  individualCellsSuppresed <- length(which(!is.na(modifications$row) & !is.na(modifications$col) & is.na(modifications$value) )) #+ (suppressedInds*ncol(object$data$geno)) + (suppressedMarkers*nrow(object$data$geno))
   imputedCells <- length(which(!is.na(modifications$row) & !is.na(modifications$col) & !is.na(modifications$value) ))
   final <- data.frame(suppressedMarkers=suppressedMarkers,
-                      suppressedInds=suppressedInds, totalSuppressedCells=suppressedCells,
+                      suppressedInds=suppressedInds, individualCellsSuppresed=individualCellsSuppresed,
                       imputedCells=imputedCells)
   return(final)
   
