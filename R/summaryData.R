31ebd6e3303f025d9eeafd977ee46c89192ca2f8
covaruber <cova_ruber@live.com.mx>
2024-09-22 21:52:44 -0500

    added data summary function

--- R/summaryData.R
+++ R/summaryData.R
@@ -1,6 +1,11 @@
 summaryData <- function(object){
   nFeatures = unlist(lapply(object$data, ncol))
+  mapped = unlist(lapply(object$metadata, nrow))
   nRecords = unlist(lapply(object$data, nrow))
-  res = data.frame(nFeatures=nFeatures, nRecords=nRecords )
+  nMods = unlist(lapply(object$modifications, nrow))
+  res = data.frame(nFeatures=nFeatures, mapped=mapped, nRecords=nRecords )
+  res$nDataPoints = res$mapped * res$nRecords
+  res$modifications <- NA
+  res[names(nMods),"modifications"] = nMods
   return(res)
 }
