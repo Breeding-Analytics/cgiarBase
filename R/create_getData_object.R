e37a4fcba3b03d98a9350797f166ef5becf69b3e
covaruber <Cova@MacBook-Pro.local>
2024-11-05 22:11:02 +0800

    added effecttype column

--- R/create_getData_object.R
+++ R/create_getData_object.R
@@ -1,10 +1,10 @@
 create_getData_object <- function() {
   obj <- list()
-  
+
   obj$data <- list()
   obj$metadata <- list()
   obj$modifications <- list()
-  
+
   obj$data$pheno    <- data.frame()
   obj$data$geno     <- matrix()
   obj$data$weather  <- data.frame()
@@ -13,48 +13,48 @@ create_getData_object <- function() {
                                   father = character(),
                                   yearOfOrigin = integer())
   obj$data$qtl     <- matrix()
-  
+
   obj$metadata$pheno <- data.frame(parameter = character(),
                                    value = character())
-  
+
   obj$metadata$geno <- data.frame(marker = character(),
                                   chr = character(),
                                   pos = double(),
                                   refAllele = character(),
                                   altAllele = character())
-  
+
   obj$metadata$pedigree <- data.frame(parameter = character(),
                                       value = double())
-  
+
   obj$metadata$weather <- data.frame(parameter = character(),
                                      value = double())
-  
+
   obj$metadata$qtl <- data.frame(parameter = character(),
                                  value = double())
-  
+
   obj$modifications$pheno <- data.frame(module = character(),
                                         analysisId = character(),
                                         trait = character(),
                                         reason = character(),
                                         row = integer(),
                                         value = double())
-  
+
   obj$modifications$geno <- data.frame(module = character(),
                                        analysisId = character(),
                                        reason = character(),
                                        row = integer(),
                                        col = integer(),
                                        value = double())
-  
+
   obj$modifications$pedigree <- data.frame(module = character(),
                                            analysisId = character(),
                                            reason = character(),
                                            row = integer())
-  
+
   obj$modifications$weather <- data.frame()
-  
+
   obj$modifications$qtl <- data.frame()
-  
+
   obj$predictions <- data.frame(module = character(),
                                 analysisId = character(),
                                 pipeline = character(),
@@ -63,12 +63,13 @@ create_getData_object <- function() {
                                 designation = character(),
                                 mother = character(),
                                 father = character(),
+                                effectType = character(),
                                 entryType = character(),
                                 environment = character(),
                                 predictedValue = double(),
                                 stdError = double(),
                                 reliability = double())
-  
+
   obj$metrics <- data.frame(module = character(),
                             analysisId = character(),
                             trait = character(),
@@ -77,16 +78,16 @@ create_getData_object <- function() {
                             method = character(),
                             value = double(),
                             stdError = double())
-  
+
   obj$modeling <- data.frame(module = character(),
                              analysisId = character(),
                              trait = character(),
                              environment = character(),
                              parameter = character(),
                              value = character())
-  
+
   obj$status <- data.frame(module = character(),
                            analysisId = character())
-  
+
   return(obj)
 }
